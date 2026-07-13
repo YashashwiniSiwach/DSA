746a125a0efa1aa3c57d86293c901a8d424c5e2e
YashashwiniSiwach <yashashwinisiwach26@gmail.com>
2026-07-13 18:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (18.34%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int num=start^goal;
+        int cnt=0;
+        for(int i=0;i<32;i++){
+            if((num & (1<<i))!=0){
+                cnt=cnt+1;
+            }
+        }
+        return cnt;
+    }
+}
