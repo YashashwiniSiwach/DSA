8952f41f5964dce3429603f83e97cf5a184d8e3a
YashashwiniSiwach <yashashwinisiwach26@gmail.com>
2026-07-30 21:57:22 +0530

    Time: 2 ms (99.71%) | Memory: 45.5 MB (76.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -4,7 +4,7 @@ class Solution {
 
         int[] need = new int[128];
 
-        // Count required characters
+
         for (char c : t.toCharArray()) {
             need[c]++;
         }
@@ -18,14 +18,13 @@ class Solution {
         while (right < s.length()) {
             char r = s.charAt(right);
 
-            // If character is needed, reduce count
             if (need[r] > 0) {
                 count--;
             }
             need[r]--;
             right++;
 
-            // When all characters are found
+
             while (count == 0) {
                 if (right - left < minLen) {
                     minLen = right - left;
@@ -34,9 +33,7 @@ class Solution {
 
                 char l = s.charAt(left);
                 need[l]++;
-
-                // If removing this character makes window invalid
-                if (need[l] > 0) {
+              if (need[l] > 0) {
                     count++;
                 }
 
