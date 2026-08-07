0c6b4ecac07c6a223414a90fcf0b18fa3fd41eee
YashashwiniSiwach <yashashwinisiwach26@gmail.com>
2026-08-07 18:41:43 +0530

    Time: 2 ms (88.76%) | Memory: 47.9 MB (63.39%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int farthest = 0;
+          int farthest = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (i > farthest) {
