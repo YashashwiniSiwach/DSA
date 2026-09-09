0f0a2b41b6e29ccef6605b8f79a30428aa8b5ed1
YashashwiniSiwach <yashashwinisiwach26@gmail.com>
2026-09-09 16:21:37 +0530

    Time: 4 ms (93.11%) | Memory: 46.8 MB (52.71%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>adj= new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int [] p: prerequisites){
+            int courses =p[0];
+            int prerequisite=p[1];
+
+        adj.get(prerequisite).add(courses);
+        }
+        int [] vis=new int [numCourses];
+        Stack<Integer> st= new Stack<>();
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){
+                if(!dfs(i,adj,vis,st)){
+                    return new int [0];
+                }
+            }
+        }
+        int [] result= new int [numCourses];
+        for(int i=0;i<numCourses;i++){
+            result[i]=st.pop();
+        }
+        return result;
+
+    }
+    private boolean dfs(int node,
+        List<List<Integer>> adj,
+        int[] visited,
+        Stack<Integer> stack
+    ) {
+
+        // Cycle detected
+        if (visited[node] == 1) {
+            return false;
+        }
+
+        // Already completely processed
+        if (visited[node] == 2) {
+            return true;
+        }
+
+        // Mark as currently visiting
+        visited[node] = 1;
+
+        // Visit neighbours
+        for (int next : adj.get(node)) {
+
+            if (!dfs(next, adj, visited, stack)) {
+                return false;
+            }
+        }
+
+        // Mark as completely visited
+        visited[node] = 2;
+
+        // Add AFTER all neighbours
+        stack.push(node);
+
+        return true;
+    }
+}
+    
